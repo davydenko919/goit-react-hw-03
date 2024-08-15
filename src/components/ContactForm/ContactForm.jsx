d298cd121dffd686b21c93c1882bae80f6c2223a
davydenko919 <davydenko919@gmail.com>
2024-08-15 13:24:17 +0300

    fSave

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,22 +1,41 @@
 import css from "./ContactForm.module.css";
 import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import { ErrorMessage } from "formik";
+import { useId } from "react";
 
 export default function ContactForm({ HendleSubmit }) {
+  const FormSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(3, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    number: Yup.string()
+      .min(3, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+  });
+
+  const nameFieldId = useId();
+  const numberFieldId = useId();
   return (
     <>
       <Formik
-        initialValues={{ username: "", email: "" }}
+        initialValues={{ id: "", name: "", number: "" }}
         onSubmit={HendleSubmit}
+        validationSchema={FormSchema}
       >
-        <Form>
-          <div>
-            <label>Name</label>
-            <Field name="username"></Field>
+        <Form className={css.container}>
+          <div className={css.itemContainer}>
+            <label htmlFor={nameFieldId}>Name</label>
+            <Field name="name" id={nameFieldId}></Field>
           </div>
+          <ErrorMessage name="name" component="span" />
           <div>
-            <label>Namber</label>
-            <Field name="email"></Field>
+            <label htmlFor={numberFieldId}>Namber </label>
+            <Field name="number" id={numberFieldId}></Field>
           </div>
+          <ErrorMessage name="number" component="span" />
 
           <button type="submit">Add contact</button>
         </Form>
